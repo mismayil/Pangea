d0f201c4cf8acafdfd59f3774cfead34e2633bb1
Yueqi <yueqis@andrew.cmu.edu>
2024-11-03 16:29:31 -0500

    xgqa+marvl

--- evaluation/lmms-eval/lmms_eval/tasks/xgqa/utils.py
+++ evaluation/lmms-eval/lmms_eval/tasks/xgqa/utils.py
@@ -1,19 +1,6 @@
-from datasets import load_dataset
-
-XGQA_RAW_IMAGE_DATASET = None
-XGQA_ID2IMAGE = None
-
 
 def xgqa_doc_to_visual(doc):
-    global XGQA_RAW_IMAGE_DATASET
-    global XGQA_ID2IMAGE
-    if XGQA_RAW_IMAGE_DATASET is None:
-        XGQA_RAW_IMAGE_DATASET = load_dataset("lmms-lab/GQA", "testdev_balanced_images", split="testdev", token=True)
-        XGQA_ID2IMAGE = {}
-        for row in XGQA_RAW_IMAGE_DATASET:
-            XGQA_ID2IMAGE[row["id"]] = row["image"].convert("RGB")
-    image = XGQA_ID2IMAGE[doc["image_id"]]
-    return [image]
+    return [doc['image'].convert("RGB")]
 
 
 def xgqa_doc_to_text(doc, model_specific_prompt_kwargs):
@@ -24,7 +11,6 @@ def xgqa_doc_to_text(doc, model_specific_prompt_kwargs):
 
 def xgqa_process_result(doc, results):
     target = doc['answer'].strip().lower()
-    # print(f"results[0]: {results[0]}")
     pred = results[0]
     pred = pred.strip().lower()
     if target in pred:
